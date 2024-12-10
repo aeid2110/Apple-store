8ee7b7d1d29824d6eebe042a8d57b6c0562fdf63
aeid2110 <aeid5107@gmail.com>
2024-12-10 17:28:07 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+let img =document.querySelector('.img')
+let all =document.querySelector('.all')
+function phones(phone){
+    img.src = phone;
+}
+function colors(color){
+    all.style.background = color;
+}
